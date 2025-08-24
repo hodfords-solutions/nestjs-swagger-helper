31e36a38561776e747952c2a62842bbb86cc3b9e
Minh Ngo HF <minh.ngo@hodfords.com>
2025-08-24 14:24:58 +0700

    feat: support api permission decorator

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,7 @@ export * from './decorators/api-param-global.decorator';
 export * from './decorators/api-public-controller.decorator';
 export * from './decorators/api-public.decorator';
 export * from './decorators/api-set-value.decorator';
+export * from './decorators/api-permissions.decorator';
 export * from './services/swagger-helper.service';
 export * from './swagger-helper.module';
 export * from './types/swagger-initialization.type';
